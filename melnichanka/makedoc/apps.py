1d602c92e3a757f41927ae513ef0a34235fa2439
Lymar Volodymyr <shaxtar11@gmail.com>
2023-12-27 20:47:41 +0100

    move teamplates

--- melnichanka/makedoc/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MakedocConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "makedoc"
--- melnichanka/voiceconvert/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VoiceconvertConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "voiceconvert"
