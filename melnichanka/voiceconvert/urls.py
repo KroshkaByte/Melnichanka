903e191d364280f72ef1df5b11da36abddc1c46a
Lymar Volodymyr <shaxtar11@gmail.com>
2023-12-28 16:33:35 +0100

    __

--- melnichanka/voiceconvert/urls.py
+++ melnichanka/voiceconvert/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home_view, name="home"),
-    path('recognize_speech/', views.recognize_speech_view, name='recognize_speech'),
+    path("recognize_speech/", views.recognize_speech_view, name="recognize_speech"),
     path("signup/", views.SignUpView.as_view(), name="signup"),
 ]
