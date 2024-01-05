401e1fa30ab7d481c4282eb736adfde6279785bb
leeroymk <demonic90@mail.ru>
2024-01-05 13:41:45 +0300

    modified settings.py for debugging

--- melnichanka/melnichanka/settings.py
+++ melnichanka/melnichanka/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-if DEBUG:
-    load_dotenv()
+load_dotenv()
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
