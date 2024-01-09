30d9b96073cc2956fde089d33ad8b3f855c7abfa
Antoine <47349260+leeroymk@users.noreply.github.com>
2024-01-09 21:43:12 +0300

    Tony1 (#11)
    * reorganized project, new requirements.txt
    
    * modified settings.py for debugging
    
    * new handlers 404 and 500
    
    * phones added to user model, added new model factory
    
    * new migrations, new fields in models.py
    
    * new models
    
    ---------
    
    Co-authored-by: leeroymk <demonic90@mail.ru>

--- melnichanka/voiceconvert/admin.py
+++ melnichanka/voiceconvert/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import UserChangeForm, UserCreationForm
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import User
 
 
 class CustomUserAdmin(UserAdmin):
-    add_form = UserCreationForm
-    form = UserChangeForm
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model = User
     list_display = [
         "email",
--- melnichanka/voiceconvert/urls.py
+++ melnichanka/voiceconvert/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
     path("", views.home_view, name="home"),
-    path("recognize_speech/", views.recognize_speech_view, name="recognize_speech"),
     path("signup/", views.SignUpView.as_view(), name="signup"),
 ]
