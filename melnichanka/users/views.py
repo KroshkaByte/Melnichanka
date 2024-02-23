8d9af6200ac3aad942dc694b1ac003a505f9c151
Lymar Volodymyr <shaxtar11@gmail.com>
2024-02-23 21:00:06 +0100

    prohibiting editing another user's data

--- melnichanka/users/views.py
+++ melnichanka/users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, exceptions
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
@@ -6,3 +6,12 @@ from .serializers import CustomUserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        obj = super().get_object()
+        if self.request.user != obj:
+            raise exceptions.PermissionDenied(
+                "Вы не можете редактировать данные другого пользователя"
+            )
+        return obj
