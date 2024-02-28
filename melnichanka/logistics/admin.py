7b78dae3bdb3fe04f63ff7e1989fced182f3d697
leeroymk <demonic90@mail.ru>
2024-02-28 11:40:43 +0300

    logistics admin modified

--- melnichanka/logistics/admin.py
+++ melnichanka/logistics/admin.py
@@ -1,14 +1,34 @@
 from django.contrib import admin
 
-from .models import TripsAuto, TripsRailway
+from .models import TripsAuto, TripsRailway, City, RailwayStations
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ["city", "region", "federal_district"]
+    list_display_links = ["city", "region", "federal_district"]
+    ordering = ["city"]
+    list_per_page = 20
+    search_fields = ["city", "region", "federal_district"]
+    list_filter = ["city", "region", "federal_district"]
+
+
+@admin.register(RailwayStations)
+class RailwayStationsAdmin(admin.ModelAdmin):
+    list_display = ["station_name", "station_id", "station_branch"]
+    list_display_links = ["station_name", "station_id", "station_branch"]
+    ordering = ["station_name"]
+    list_per_page = 20
+    search_fields = ["station_name", "station_id", "station_branch"]
+    list_filter = ["station_name", "station_id", "station_branch"]
 
 
 @admin.register(TripsAuto)
 class AutoAdmin(admin.ModelAdmin):
     list_display = ["departure_city", "destination_city", "cost_per_tonn_auto"]
-    list_display_links = ["destination_city"]
+    list_display_links = ["departure_city", "destination_city"]
     ordering = ["departure_city", "destination_city"]
-    list_editable = ["departure_city", "cost_per_tonn_auto"]
+    list_editable = ["cost_per_tonn_auto"]
     list_per_page = 20
     search_fields = ["departure_city", "destination_city"]
     list_filter = ["departure_city", "destination_city"]
