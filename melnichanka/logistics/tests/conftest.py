2bd8f3781ebc5f1b0b1570d45ea6befd33cb50a8
leeroymk <demonic90@mail.ru>
2024-03-20 15:03:03 +0300

    removed unnecessary tests goods, logistics

--- melnichanka/logistics/tests/conftest.py
+++ melnichanka/logistics/tests/conftest.py
@@ -1,21 +1,6 @@
 import pytest
-from logistics.models import City, RailwayStations, TripsAuto, TripsRailway
 
-
-@pytest.fixture
-def city_object():
-    return City.objects.create(
-        city="name_city", region="name_region", federal_district="name_federal_district"
-    )
-
-
-@pytest.fixture
-def city_object_second():
-    return City.objects.create(
-        city="name_city_second",
-        region="name_region_second",
-        federal_district="name_federal_district_second",
-    )
+from logistics.models import RailwayStations, TripsAuto, TripsRailway
 
 
 @pytest.fixture
@@ -33,15 +18,6 @@ def railwaystation_object():
         station_name="name_station",
         station_id=1000,
         station_branch="name_branch",
-    )
-
-
-@pytest.fixture
-def railwaystation_object_second():
-    return RailwayStations.objects.create(
-        station_name="name_station_second",
-        station_id=1000,
-        station_branch="name_branch_second",
     )
 
 
