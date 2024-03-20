8c674cabd4bdf65f2a0b38cc41dfca53a4ac7549
leeroymk <demonic90@mail.ru>
2024-03-20 14:53:45 +0300

    trying to add nginx

--- melnichanka/entrypoint.sh
+++ melnichanka/entrypoint.sh
@@ -3,9 +3,14 @@
 echo "Сборка статических файлов"
 python3 manage.py collectstatic --noinput
 
+echo "Создание миграций"
+python3 manage.py makemigrations
+
 echo "Миграция базы данных"
 python3 manage.py migrate
 
-echo "Запуск веб-сервера"
+echo "Запуск nginx"
 exec nginx -c /etc/nginx/nginx.conf
 
+echo "Запуск gunicorn"
+exec gunicorn melnichanka.wsgi:application --config gunicorn.conf.py --bind 0.0.0.0:8000
