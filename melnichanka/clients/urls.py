3c06e0733409544b925d3f4a626e56369f5fcca7
Lymar Volodymyr <shaxtar11@gmail.com>
2024-03-26 00:10:38 +0100

    fix director position urls, views, serializ

--- melnichanka/clients/urls.py
+++ melnichanka/clients/urls.py
@@ -4,6 +4,7 @@ from .views import (
     ClientAPIDeleteView,
     ClientAPIUpdateView,
     ClientsAPIView,
+    DirectorPositionListView,
 )
 
 
@@ -11,4 +12,9 @@ urlpatterns = [
     path("", ClientsAPIView.as_view(), name="clients"),
     path("<int:pk>/", ClientAPIUpdateView.as_view(), name="clients_update"),
     path("delete/<int:pk>/", ClientAPIDeleteView.as_view(), name="clients_delete"),
+    path(
+        "directorposition/",
+        DirectorPositionListView.as_view(),
+        name="director_position",
+    ),
 ]
--- melnichanka/clients/views.py
+++ melnichanka/clients/views.py
@@ -3,19 +3,28 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 
 from .services import BaseView
-from .models import Clients
-from .serializers import ClientSerializer
+from .models import Clients, Director_position
+from .serializers import ClientSerializer, DirectorpositionSerializer
 
 
+# Базовый класс  для получения данных по записям клиентов
 class ClientsAPIView(generics.ListCreateAPIView):
     queryset = Clients.objects.all()
     serializer_class = ClientSerializer
     permission_classes = (IsAuthenticated,)
 
 
+# Изменение данных записи клиента
 class ClientAPIUpdateView(BaseView, generics.RetrieveUpdateAPIView):
     pass
 
 
+# Удаление данных записи клиента
 class ClientAPIDeleteView(BaseView, generics.DestroyAPIView):
     pass
+
+
+# Передача списка позиций директора для фронтенда
+class DirectorPositionListView(generics.ListAPIView):
+    queryset = Director_position.objects.all()
+    serializer_class = DirectorpositionSerializer
