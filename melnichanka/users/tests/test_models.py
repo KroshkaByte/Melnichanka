35f754843bb855a3da6080c9310dba8a11ed0ab3
Lymar Volodymyr <shaxtar11@gmail.com>
2024-03-25 10:18:18 +0100

    fix users tests

--- melnichanka/users/tests/test_models.py
+++ melnichanka/users/tests/test_models.py
@@ -6,30 +6,8 @@ from users.models import CustomUser, Department, Position
 
 
 @pytest.mark.django_db
-def test__position__create_object_valid(position_object):
-    assert position_object.position == "position"
-
-
-@pytest.mark.django_db
-def test__position__get_object_valid(position_object):
-    retrieved_position = Position.objects.get(id=position_object.id)
-    assert retrieved_position.position == position_object.position
-
-
-@pytest.mark.django_db
 def test__position__return_valid_str(position_object):
     assert str(position_object) == "position"
-
-
-@pytest.mark.django_db
-def test__department__create_object_valid(department_object):
-    assert department_object.department == "department"
-
-
-@pytest.mark.django_db
-def test__department__get_object_valid(department_object):
-    retrieved_department = Department.objects.get(id=department_object.id)
-    assert retrieved_department.department == department_object.department
 
 
 @pytest.mark.django_db
