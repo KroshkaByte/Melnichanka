bd5ab4b15ec0ed0685c8e481568555773bda02c5
Lymar Volodymyr <shaxtar11@gmail.com>
2024-03-25 09:28:20 +0100

    added new model director_position, fix admin clients, users

--- melnichanka/clients/admin.py
+++ melnichanka/clients/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Clients
+from .models import Clients, Director_position
+
+
+admin.site.register(Director_position)
 
 
 @admin.register(Clients)
