687079b76fa2d70c69afa113151457fa96d6c19c
Lymar Volodymyr <shaxtar11@gmail.com>
2024-03-25 10:15:18 +0100

    fix clients tests

--- melnichanka/clients/tests/test_views.py
+++ melnichanka/clients/tests/test_views.py
@@ -1,40 +1,19 @@
 import pytest
 
-from django.test import RequestFactory
-from rest_framework.test import force_authenticate
-from rest_framework_simplejwt.tokens import RefreshToken
-from clients.views import ClientsAPIView, ClientAPIUpdateView, ClientAPIDeleteView
-from users.models import CustomUser
-
 
 @pytest.mark.django_db
-def test__client_api_view__list_object_valid(clients_object):
-    user = CustomUser.objects.create_user(
-        email="testuser@test.com", full_name="Test User", password="testpass"
-    )
-    refresh = RefreshToken.for_user(user)
-    factory = RequestFactory()
-    request = factory.get("/")
-    force_authenticate(request, user=user, token=refresh)
-    response = ClientsAPIView.as_view()(request)
+def test__client_api_view__list_object_valid(authorized_client, clients_object):
+    response = authorized_client.get("/api/v1/clients/")
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test__client_api_view__update_object_valid(clients_object, user):
-    refresh = RefreshToken.for_user(user)
-    factory = RequestFactory()
-    request = factory.get("/")
-    force_authenticate(request, user=user, token=refresh)
-    response = ClientAPIUpdateView.as_view()(request, pk=clients_object.pk)
+def test__client_api_view__update_object_valid(authorized_client, clients_object):
+    response = authorized_client.get(f"/api/v1/clients/{clients_object.pk}/")
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test__client_api_view__delete_object_valid(clients_object, user):
-    refresh = RefreshToken.for_user(user)
-    factory = RequestFactory()
-    request = factory.delete("/")
-    force_authenticate(request, user=user, token=refresh)
-    response = ClientAPIDeleteView.as_view()(request, pk=clients_object.pk)
+def test__client_api_view__delete_object_valid(authorized_client, clients_object):
+    response = authorized_client.delete(f"/api/v1/clients/delete/{clients_object.pk}/")
     assert response.status_code == 204
