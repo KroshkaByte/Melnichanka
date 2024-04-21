fe14702b038a7a9f36ed697f2825dd13781afc3e
Volodymyr Lymar <133962299+KroshkaByte@users.noreply.github.com>
2024-04-21 23:22:46 +0200

    Kroshkadoc (#47)
    * removed unnecessary lib
    
    * add platform
    
    * create fakerscript for db, small fix makedoc views, del platform from githubactions
    
    * del platform from githubactions
    
    * small fix scripts
    
    * Added new library(pymorphy3), removed unnecessary file(makedoc/constants), improved readability by moving functions from views to services, fix urls
    
    * add ru locale

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,9 @@ ENV PYTHONUNBUFFERED 1
 
 COPY requirements.txt /app/
 
+RUN apt-get clean && apt-get update && apt-get install -y locales
+RUN locale-gen ru_RU.UTF-8
+
 RUN pip install --upgrade pip \
  && pip install -r requirements.txt
 
--- makedoc/urls.py
+++ makedoc/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path("home/", views.HomeView.as_view(), name="home"),
-    path("filemake_auto/", views.write_to_excel_auto, name="file"),
-    path("filemake_rw/", views.write_to_excel_rw, name="file"),
+    path("filemake/", views.create_docs, name="file"),
 ]
