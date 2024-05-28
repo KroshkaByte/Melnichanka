43ea23a0c0a97ea5cc5a31e19ed24a42619556b5
Lymar Volodymyr <shaxtar11@gmail.com>
2024-05-28 11:11:38 +0200

    Fixed the display of the product list

--- goods/serializers.py
+++ goods/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
-
 from .models import Product
 
 
-class GoodsSerializer(serializers.ModelSerializer[Product]):
+class GoodsSerializer(serializers.ModelSerializer):
+    flour_name = serializers.StringRelatedField()
+    brand = serializers.StringRelatedField()
+    package = serializers.StringRelatedField()
+
     class Meta:
         model = Product
         fields = "__all__"
