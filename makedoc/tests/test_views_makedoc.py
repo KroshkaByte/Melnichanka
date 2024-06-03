a0836001ecedfbd392740f8a5968aca79d9e60a1
Lymar Volodymyr <shaxtar11@gmail.com>
2024-06-03 16:12:29 +0200

    Added tests for dataview

---
+++ makedoc/tests/test_views_makedoc.py
@@ -0,0 +1,35 @@
+import pytest
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+
+@pytest.mark.django_db
+def test_data_doc_view_status_code(api_client: APIClient) -> None:
+    url = reverse('data')
+    data = {
+        "client_id": 123,
+        "items": [
+            {"product_id": 1, "quantity": 2, "discount": 10},
+            {"product_id": 2, "quantity": 5, "discount": 4}
+        ],
+        "factory_id": 1,
+        "destination": "New York"
+    }
+    response = api_client.post(url, data, format='json')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_data_doc_view_response_data(api_client: APIClient) -> None:
+    url = reverse('data')
+    data = {
+        "client_id": 123,
+        "items": [
+            {"product_id": 1, "quantity": 2, "discount": 10},
+            {"product_id": 2, "quantity": 5, "discount": 4}
+        ],
+        "factory_id": 1,
+        "destination": "New York"
+    }
+    response = api_client.post(url, data, format='json')
+    assert response.json() == data
