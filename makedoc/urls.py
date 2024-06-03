654ecf46b61e69fb3fa139a51bc24c16bf4efe9f
Lymar Volodymyr <shaxtar11@gmail.com>
2024-06-03 15:54:26 +0200

    Added Srializers for data docs, create view, url

--- makedoc/urls.py
+++ makedoc/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import DataDocView
 
 urlpatterns = [
     path("filemake/", views.create_docs, name="file"),
+    path("data/", DataDocView.as_view(), name="data"),
 ]
--- makedoc/views.py
+++ makedoc/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from rest_framework import generics, status
+from rest_framework.response import Response
 
+from .serializers import DataDocSerializer
 from .services import write_to_excel_auto, write_to_excel_rw, write_to_excel_sluzebnyi
 
 
@@ -9,3 +12,15 @@ def create_docs(request):
     # Если применяется скидка, пишем служебную записку
     write_to_excel_sluzebnyi(request) if 0 else None
     return HttpResponse("Документы сохранены")
+
+
+class DataDocView(generics.GenericAPIView):  # type: ignore
+    serializer_class = DataDocSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        validated_data = serializer.validated_data
+
+        return Response(validated_data, status=status.HTTP_200_OK)
