e7927e3e281eaed573a2a152d95fc8fc3affccd6
Lymar Volodymyr <shaxtar11@gmail.com>
2024-06-03 11:58:39 +0200

    added cache for goods list

--- melnichanka/settings.py
+++ melnichanka/settings.py
@@ -128,7 +128,7 @@ CACHES = {
         "LOCATION": f"redis://{REDIS_HOST}:{REDIS_PORT}/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        },
     }
 }
 
