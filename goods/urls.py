2aff96b59c35847cddab72da7bffc93813dc4626
Lymar Volodymyr <shaxtar11@gmail.com>
2024-06-06 20:59:37 +0200

    Added list factory url, view

--- goods/urls.py
+++ goods/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import GoodsViewSet
+from .views import GoodsViewSet, FactoryListAPIView
 
 router = routers.SimpleRouter()
 router.register(r"goods", GoodsViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path("factories/", FactoryListAPIView.as_view(), name="factory-list")
+]
