05212a02d8cc1193f1eed9f9e452f8846bb035d4
Lymar Volodymyr <shaxtar11@gmail.com>
2024-06-08 22:33:23 +0200

    Added new methods get_products, get_factory for DataService class

--- makedoc/data_service.py
+++ makedoc/data_service.py
@@ -1,4 +1,6 @@
 from clients.models import Client
+from goods.models import Product
+from logistics.models import Factory
 
 
 class DataService:
@@ -31,3 +33,45 @@ class DataService:
             return client_data
         except Client.DoesNotExist:
             raise Exception("Client not found")
+
+    @staticmethod
+    def get_products(processed_data):
+        try:
+            products_data = processed_data.get("items")
+            results = []
+            for item in products_data:
+                product_id = item.get("product_id")
+                product = Product.objects.get(id=product_id)
+                product_data = {
+                    "product": product.id,
+                    "flour_name": str(product.flour_name),
+                    "brand": str(product.brand),
+                    "package": str(product.package.package),
+                    "price": str(product.price),
+                    "quantity": item.get("quantity"),
+                    "discount": item.get("discount"),
+                }
+
+                results.append(product_data)
+            return results
+        except Product.DoesNotExist:
+            raise Exception("Product not found")
+
+    @staticmethod
+    def get_factory(processed_data):
+        try:
+            factory_id = processed_data.get("factory_id")
+            factory = Factory.objects.get(id=factory_id)
+            factory_data = {
+                "id": int(factory.id),
+                "full_name": str(factory.full_name),
+                "short_name": str(factory.short_name),
+                "full_address": str(factory.full_address),
+                "departure_city": str(factory.departure_city),
+                "departure_station_branch": str(factory.departure_station_branch),
+                "departure_station_id": str(factory.departure_station_id),
+                "departure_station_name": str(factory.departure_station_name),
+            }
+            return factory_data
+        except Factory.DoesNotExist:
+            raise Exception("Factory not found")
