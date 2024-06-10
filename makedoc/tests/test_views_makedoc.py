078d47b1419c81dc4adea70e33bf8fe961b33198
Lymar Volodymyr <shaxtar11@gmail.com>
2024-06-10 09:53:50 +0200

    Fix goods, makedoc tests

--- makedoc/tests/test_views_makedoc.py
+++ makedoc/tests/test_views_makedoc.py
@@ -4,10 +4,11 @@ from rest_framework.test import APIClient
 
 
 @pytest.mark.django_db
-def test__data_view__unauthorized_user_cannot_post_data() -> None:
+def test__data_doc_view__unauthorized_user_cannot_post_data() -> None:
     client = APIClient()
     url = reverse("data")
     data = {
+        "delivery_type": ["auto", "rw"],
         "client_id": 1,
         "items": [
             {"product_id": 1, "quantity": 2, "discount": 10},
@@ -15,16 +16,17 @@ def test__data_view__unauthorized_user_cannot_post_data() -> None:
         ],
         "factory_id": 1,
         "destination": "New York",
-        "delivery_cost": 2000,
+        "delivery_cost": 1500,
     }
     response = client.post(url, data, format="json")
     assert response.status_code == 401  # Unauthorized
 
 
 @pytest.mark.django_db
-def test__goods__authorized_user_can_post_data(authorized_client) -> None:
+def test__data_doc_view__authorized_user_can_post_data(authorized_client) -> None:
     url = reverse("data")
     data = {
+        "delivery_type": ["auto", "rw"],
         "client_id": 1,
         "items": [
             {"product_id": 1, "quantity": 2, "discount": 10},
@@ -39,9 +41,10 @@ def test__goods__authorized_user_can_post_data(authorized_client) -> None:
 
 
 @pytest.mark.django_db
-def test__goods__authorized_user_post_data_response_is_correct(authorized_client) -> None:
+def test__data_doc_view__authorized_user_post_data_response_is_correct(authorized_client) -> None:
     url = reverse("data")
     data = {
+        "delivery_type": ["auto", "rw"],
         "client_id": 1,
         "items": [
             {"product_id": 1, "quantity": 2, "discount": 10},
@@ -52,4 +55,4 @@ def test__goods__authorized_user_post_data_response_is_correct(authorized_client
         "delivery_cost": 1500,
     }
     response = authorized_client.post(url, data, format="json")
-    assert response.json() == data
+    assert response.json() == {"success": True, "data": data}
