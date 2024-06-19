51a0f03c710511dfba5c66303679082454acab55
Lymar Volodymyr <shaxtar11@gmail.com>
2024-06-19 10:33:12 +0200

    feat: add download documet url and view

--- makedoc/urls.py
+++ makedoc/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import DataDocView, CreateDocsView
+from .views import DataDocView, CreateDocsView, DownloadDocView
 
 urlpatterns = [
     path("filemake/", CreateDocsView.as_view(), name="file"),
+    path("downloadxls/", DownloadDocView.as_view(), name="downloadxls"),
     path("data/", DataDocView.as_view(), name="data"),
 ]
