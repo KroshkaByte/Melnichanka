780d066afc871bbc85bb242606064296ce989f3a
Lymar Volodymyr <shaxtar11@gmail.com>
2024-06-22 12:56:09 +0200

    feat: added list user documents view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+import os.path
+
 from django.dispatch import receiver
 from django_rest_passwordreset.signals import reset_password_token_created
 from rest_framework import generics, status
@@ -68,6 +70,30 @@ class UserUpdateView(UserRelatedView):
 # Изменение пароля пользователя
 class UserUpdatePasswordView(UserRelatedView):
     serializer_class = UserUpdatePasswordSerializer
+
+
+# Отображение списка документов пользователя
+class ListUserFilesAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        user_folder = os.path.join("makedoc", "tempdoc", str(request.user.id))
+
+        if not os.path.exists(user_folder):
+            return Response({"error": "User folder not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            files = os.listdir(user_folder)
+        except OSError as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        files_list = []
+        for file_name in files:
+            file_path = os.path.join(user_folder, file_name)
+            if os.path.isfile(file_path):
+                files_list.append(file_name)
+
+        return Response({"files": files_list}, status=status.HTTP_200_OK)
 
 
 # Сброс пароля
